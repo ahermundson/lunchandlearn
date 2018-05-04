1a734f7f201d8522c0b12def0cf2a394378c800c
21STSERVICES\ahermundson <ahermundson@itm21st.com>
2018-05-04 16:52:33 -0500

    Updated Images trhrough arrow functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Landing from './Landing';
 import LetVsVar from './LetVsVar';
 import ArrowFunctions from './ArrowFunctions';
@@ -8,18 +9,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>ES6 Lunch and Learn</h1>
-        </header>
-        <HashRouter>
-          <div>
-            <Route exact path="/" component={Landing} />
-            <Route path="/letvsvar" component={LetVsVar} />
-            <Route path="/arrowFunctions" component={ArrowFunctions} />
-          </div>
-        </HashRouter>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <h1>ES6 Lunch and Learn</h1>
+          </header>
+          <HashRouter>
+            <div>
+              <Route exact path="/" component={Landing} />
+              <Route path="/letvsvar" component={LetVsVar} />
+              <Route path="/arrowFunctions" component={ArrowFunctions} />
+            </div>
+          </HashRouter>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/ArrowFunctions.js
+++ src/ArrowFunctions.js
@@ -1,104 +1,123 @@
 /* eslint no-unused-vars: 0 */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import FlatButton from 'material-ui/FlatButton';
 
 function ArrowFunctionPerson() {
-  this.age = 5;
-
-  setTimeout(() => {
-    console.log('Arrow Function: ', this.age);
-  }, 0);
+  this.age = 33;
+  this.name = 'Alex Hermundson';
+  this.showGreeting = false;
+  this.greeting = `Hi my name is ${this.name} and I am ${this.age} years old.`;
+  this.showGreetingClick = () => {
+    console.log(this.greeting);
+    setTimeout(() => {
+      this.hideGreeting(); 
+    }, 2000);
+  };
+  this.hideGreeting = () => {
+    console.log('Hide the greeting');
+  }
 }
 
 function Person() {
-  this.age = 5;
-
-  setTimeout(function() {
-    console.log('ES5 Function: ', this.age);
-  }, 0);
+  this.age = 33;
+  this.name = 'Alex Hermundson';
+  this.showGreeting = false;
+  this.greeting = `Hi my name is ${this.name} and I am ${this.age} years old.`;
+  this.showGreetingClick = function () {
+    console.log(this);
+    setTimeout(function () {
+      this.hideGreeting(); // => TypeError: this.hideGreeting is not a function
+    }, 2000);
+  };
+  this.hideGreeting = function () {
+    console.log('hide the greeting');
+  }
 }
 
-function es5FuncThis() {
-  const person = new Person();
-}
+const es5Person = new Person();
 
-function arrowFuncThis() {
-  const person = new ArrowFunctionPerson();
-}
+const es6Person = new ArrowFunctionPerson();
 
 const ArrowFunctions = () => (
-  <div>
-    <h1>Arrow Functions</h1>
-    <div
-      style={{
-        marginTop: '25px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}
-    >
-      <div
-        style={{
-          marginTop: '10px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        <h4>ES5 Function Syntax</h4>
-        <img src="/img/es5FunctionDeclaration.png" alt="ES5FuncDeclarations" />
+  <div style={{height: '100%'}}>
+    <Tabs>
+      <Tab label="Syntax">
+        <div
+          style={{
+            marginTop: '10px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+        >
+          <h4>ES5 Function Syntax</h4>
+          <img src="/img/es5FunctionDeclaration.png" alt="ES5FuncDeclarations" />
 
-        <h4 style={{ marginTop: '25px' }}>Arrow Function Sytax</h4>
-        <img
-          src="/img/arrowFunctionDeclaration.png"
-          alt="ES5FuncDeclarations"
-        />
-
-        <h4 style={{ marginTop: '25px' }}>Array Methods</h4>
-        <img src="/img/arrowFunctionArrays.png" alt="arrowFunctionArrays" />
-      </div>
-
-      <div
-        style={{
-          marginTop: '25px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        <h4>Variations</h4>
-        <img
-          src="/img/arrowFunctionVariations.png"
-          alt="arrowFunctionVariations"
-        />
-      </div>
-      <div
-        style={{
-          marginTop: '25px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        <h4>Arrow Functions & This</h4>
-        <img src="/img/es5FunctionThis.png" alt="es5FunctionThis" />
-        <button onClick={es5FuncThis} style={{ marginTop: '25px' }}>
-          Run Code
-        </button>
-
-        <img
-          src="/img/arrowFunctionThis.png"
-          alt="arrowFunctionThis"
-          style={{ marginTop: '25px' }}
-        />
-        <button onClick={arrowFuncThis} style={{ marginTop: '25px' }}>
-          Run Code
-        </button>
-      </div>
-    </div>
-    <Link to="/arrowFunctions" style={{ color: '#F1F7EE', textAlign: 'right' }}>
-      <h4 style={{ marginRight: '15px' }}>Next: Template Literals</h4>
-    </Link>
+          <h4 style={{ marginTop: '25px' }}>Arrow Function Sytax</h4>
+          <img
+            src="/img/arrowFunctionDeclaration.png"
+            alt="ES5FuncDeclarations"
+          />
+        </div>
+      </Tab>
+      <Tab label="Arrow Function w/ Array Methods">
+        <div
+          style={{
+            marginTop: '10px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+        >
+          <h4 style={{ marginTop: '25px' }}>Arrow Function w/ Array Methods</h4>
+          <img src="/img/arrowFunctionArrays.png" alt="arrowFunctionArrays" />
+        </div>
+      </Tab>
+      <Tab label="Variations">
+        <div
+          style={{
+            marginTop: '25px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+        >
+          <h4>Variations</h4>
+          <img
+            src="/img/arrowFunctionVariations.png"
+            alt="arrowFunctionVariations"
+          />
+        </div>
+      </Tab>
+      <Tab label="This">
+        <div
+          style={{
+            marginTop: '25px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <h4>Arrow Functions & This</h4>
+          <img src="/img/es5FunctionThis.png" alt="es5FunctionThis" />
+          <FlatButton backgroundColor="#fff" onClick={es5Person.showGreetingClick} label="Show Greeting" style={{ marginTop: '25px' }} />
+          <img
+            src="/img/arrowFunctionThis.png"
+            alt="arrowFunctionThis"
+            style={{ marginTop: '25px' }}
+          />
+          <FlatButton backgroundColor="#fff" onClick={es6Person.showGreetingClick} label="Show Greeting" style={{ marginTop: '25px' }} />
+          <Link to="/arrowFunctions" style={{ color: '#F1F7EE', marginLeft: 'auto' }}>
+            <h4 style={{ marginRight: '15px' }}>Next: Template Literals</h4>
+          </Link>
+        </div>
+      </Tab>
+    </Tabs>
   </div>
 );
 
